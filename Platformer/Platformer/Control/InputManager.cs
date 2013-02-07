ad28f52eadc5d0cc9f2c6a2a84ed847338deb8a0
Ryan RC <murphyslaw480@gmail.com>
2013-02-07 10:09:31 -0500

    Add basic inputmanager.
    Any control classes requiring input should read input by checking the
    instance of IputManager passed into their update method, NOT by directly
    checking the Keyboard/Mouse.
    
    Reason: If we modify controls, we will only have to change them in one
    place, rather than in every class where that control is read.

--- Platformer/Platformer/Control/InputManager.cs
+++ Platformer/Platformer/Control/InputManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework.Input;
+
 namespace Platformer.Control
 {
     class InputManager
@@ -11,15 +13,71 @@ namespace Platformer.Control
         #endregion
 
         #region fields
+        //Must keep track of previous state as well as current state
+        //To check if a key is held, just check IsKeyDown for the current state
+        //To check if a key was just pressed, check that key was up previously, down currently
+        KeyboardState _pastKeyState, _currentKeyState;
+        MouseState _pastMouseState, _currentMouseState;
         #endregion
 
         #region properties
+        /// <summary>
+        /// Whether the player is holding the left movement key
+        /// </summary>
+        public bool MoveLeft { get { return _currentKeyState.IsKeyDown(Keys.Left); } }
+        /// <summary>
+        /// Whether the player is holding the right movement key
+        /// </summary>
+        public bool MoveRight { get { return _currentKeyState.IsKeyDown(Keys.Right); } }
+        /// <summary>
+        /// Whether the player has tapped the left movement key
+        /// </summary>
+        public bool SelectLeft
+        {
+            get { return _currentKeyState.IsKeyDown(Keys.Left) && _pastKeyState.IsKeyUp(Keys.Left); }
+        }
+        /// <summary>
+        /// Whether the player has tapped the right movement key
+        /// </summary>
+        public bool SelectRight
+        {
+            get { return _currentKeyState.IsKeyDown(Keys.Right) && _pastKeyState.IsKeyUp(Keys.Right); }
+        }
+        /// <summary>
+        /// Whether the player has tapped the jump button
+        /// </summary>
+        public bool MoveUp
+        {
+            get { return _currentKeyState.IsKeyDown(Keys.Up) && _pastKeyState.IsKeyUp(Keys.Up); }
+        }
+        /// <summary>
+        /// Whether the player has tapped the fire button
+        /// </summary>
+        public bool Fire
+        {
+            get { return _currentKeyState.IsKeyDown(Keys.Space) && _pastKeyState.IsKeyUp(Keys.Space); }
+        }
         #endregion
 
         #region constructor
+        public InputManager()
+        {
+            Update();
+        }
         #endregion
 
         #region methods
+        /// <summary>
+        /// Game1.cs should call this every update cycle before Updating the GameState
+        /// </summary>
+        public void Update()
+        {
+            //get new input states and save old ones
+            _pastKeyState = _currentKeyState;
+            _currentKeyState = Keyboard.GetState();
+            _pastMouseState = _currentMouseState;
+            _currentMouseState = Mouse.GetState();
+        }
         #endregion
     }
 }

bc3d0b3abb6bac905e6b6f998f305835b1a30b3c
Ryan RC <murphyslaw480@gmail.com>
2013-02-07 10:48:42 -0500

    Add regions to empty classes.
    I added region declarations to the empty classes in Model, View, and
    Control. The region statements are as follows:
    
    static	- static variables and methods
    fields	- private/protected instance variables
    properties  -  public instance variables and accessors to private fields
    constructor - constructor(s) for the class
    methods - instance methods (static methods should be in the static region)
    
    These statements don't actually affect the compiled code, but they improve
    clarity and organization and allow easy code folding, so you can focus on
    just the parts of the code that you need to. Please put your code inside
    of the appropriate region.
    
    Note: Many classes may not have any static members or methods- in this
    case, you can leave this region blank.
    Also, note that the fields region is reserved for private/protected
    attributes whereas the properties region should be used for public
    attributes or properties that expose private fields (Properties are C#'s
    version of Java's get/set accessors)

--- Platformer/Platformer/Model/Gino.cs
+++ Platformer/Platformer/Model/Gino.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
 namespace Platformer.Model
 {
     class Gino : Unit
     {
+        #region static
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        #endregion
     }
 }
--- Platformer/Platformer/View/OverworldView.cs
+++ Platformer/Platformer/View/OverworldView.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace Platformer.View
 {
     class OverworldView
     {
+        #region static
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        #endregion
     }
 }

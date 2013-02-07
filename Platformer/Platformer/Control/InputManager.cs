8e236a644e309bae8545256db6978749aad9279a
Ryan RC <murphyslaw480@gmail.com>
2013-02-07 09:49:29 -0500

    Add Namespaces

---
+++ Platformer/Platformer/Control/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer.Control
+{
+    class InputManager
+    {
+        #region static
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        #endregion
+    }
+}

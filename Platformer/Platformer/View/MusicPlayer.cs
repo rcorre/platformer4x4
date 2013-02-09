bb620ab2edb8e49aa16e00a3bb16fbc3c4d3b687
Ryan RC <murphyslaw480@gmail.com>
2013-02-09 18:16:11 -0500

    Add music/sound skeleton classes

---
+++ Platformer/Platformer/View/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Platformer.View
+{
+    class MusicPlayer
+    {
+        #region static
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Start playing the specified music track
+        /// </summary>
+        /// <param name="musicName">Name of music track</param>
+        public void StartMusic(string musicName)
+        {
+            throw new NotImplementedException(); 
+        }
+        /// <summary>
+        /// Stop playing current music track
+        /// </summary>
+        public void StopMusic()
+        {
+            throw new NotImplementedException(); 
+        }
+        /// <summary>
+        /// Update MusicPlayer. Delete this method if updating every frame is not necessary
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException(); 
+        }
+        #endregion
+    }
+}
---
+++ Platformer/Platformer/View/SoundPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Platformer.View
+{
+    class SoundPlayer
+    {
+        #region static
+        #endregion
+
+        #region fields
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Start playing the specified sound effect
+        /// </summary>
+        /// <param name="musicName">Name of sound effect</param>
+        public void PlaySound(string soundName)
+        {
+            throw new NotImplementedException(); 
+        }
+        /// <summary>
+        /// Stop playing all sound effects
+        /// </summary>
+        public void StopSounds()
+        {
+            throw new NotImplementedException(); 
+        }
+        /// <summary>
+        /// Update SoundPlayer. Delete this method if updating every frame is not necessary
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException(); 
+        }
+        #endregion
+    }
+}

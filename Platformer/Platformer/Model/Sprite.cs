7e64b905698dadc28b7141df0d8317cf7ba613f5
Ryan RC <murphyslaw480@gmail.com>
2013-02-09 20:17:46 -0500

    Working on sprite

--- Platformer/Platformer/Model/Sprite.cs
+++ Platformer/Platformer/Model/Sprite.cs
@@ -13,6 +13,9 @@ namespace Platformer.Model
         #region classes
         public class SpriteSheetData
         {
+            string TextureName;
+            int SpriteWidth, SpriteHeight;
+            int NumFrames, NumStates;
         }
         #endregion
         #region static
@@ -20,6 +23,19 @@ namespace Platformer.Model
         #endregion
 
         #region fields
+        //length of each animation, in # of frames
+        int _framesPerAnimation;
+        //number of different animations, e.g. for facing different directions
+        int _numStates;
+        //time between each animation frame, and countdown to next frame
+        TimeSpan _animationInterval, _timeTillNext;
+        int _currentFrame;
+        int _currentState;
+        float _scale;
+        //organized by state, frame
+        Rectangle[,] _spriteSelectRects;
+        Color _shade;
+        Vector2 _origin;
         #endregion
 
         #region properties
